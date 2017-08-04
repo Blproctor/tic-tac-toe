808851c1e4788777de3efc1b7b900b28ce3d397f
Blproctor <Brittany.l.proctor@gmail.com>
2017-08-04 07:49:04 -0400

    Updated Tic Tac Toe Game
    Added a color sheet for more organization.  Added/modified createBoard
    function and updated index.scss for actual tic tac toe style board.

--- assets/scripts/project.js
+++ assets/scripts/project.js
@@ -1,96 +1,119 @@
 'use strict'
+const messages = $('.messages');
+const table = $('table');
+const createBoard = function () {
+  $(function () {
+    let player = 1;
+    const table = $('table');
+    console.log(messages)
+    const turn = $('.turn')
+    console.log(messages)
+    showNextPlayer(turn, player)
 
-$(function () {
-  let player = 1
-  const table = $('table')
-  const messages = $('.messages')
-  const turn = $('.square')
-  displayNextPlayer(turn, player)
-
-  $('.td').click(function () {
-    const td = $(this)
-    const state = getState(td)
-    if (!state) {
-      const pattern = definePatternForCurrentPlayer(player)
-      changeState(td, pattern)
-      if (checkIfPlayerWon(table, pattern)) {
-        messages.html('Player ' + player + ' has won.')
-        turn.html('')
+    $('td').click(function () {
+      const td = $(this)
+      console.log(messages);
+      const state = getState(td)
+      console.log(messages);
+      if (!state) {
+        let pattern = setShapeForEachPlayer(player)
+        console.log(pattern)
+        changeState(td, pattern);
+        if (checkIfPlayerWon(table, pattern)) {
+          messages.html('Player ' + player + ' has won.');
+          turn.html('');
+        } else {
+          player = setNextPlayer(player);
+          showNextPlayer(turn, player);
+        }
       } else {
-        player = setNextPlayer(player)
-        displayNextPlayer(turn, player)
+        messages.html('This box is already checked.');
       }
+    });
+
+    $('reset').click(function () {
+      player = 1
+      messages.html('');
+      reset(table);
+      showNextPlayer(turn, player)
+    })
+  })
+
+  function getState (td) {
+    if (td.hasClass('cross') || td.hasClass('circle')) {
+      return 1
     } else {
-      messages.html('This box is already checked.')
+      return 0;
     }
-  })
+  }
 
-  $('.reset').click(function () {
-    player = 1
-    messages.html('')
-    reset(table)
-    displayNextPlayer(turn, player)
-  })
-})
+  function changeState (td, pattern) {
+    if (pattern === 'cross') {
+      td.html('X')
+    } else {
+      td.html('O')
+    }
+    return td.addClass(pattern)
+  }
 
-function getState (td) {
-  if (td.hasClass('cross') || td.hasClass('circle')) {
-    return 1
-  } else {
-    return 0
+  function setShapeForEachPlayer (player) {
+    if (player === 1) {
+      return 'cross';
+    } else {
+      return 'circle';
+    }
+  }
+
+  function setNextPlayer (player) {
+    if(player == 1) {
+      return player = 2;
+    } else {
+      return player = 1;
+    }
+  }
+
+  function showNextPlayer (turn, player) {
+    messages.html('Player turn : ' + player)
   }
 }
-
-function changeState (td, pattern) {
-  return td.addClass(pattern)
-}
-
-function definePatternForCurrentPlayer (player) {
-  if (player === 1) {
-    return 'cross'
-  } else {
-    return 'circle'
-  }
-}
-
-function setNextPlayer (player) {
-  if (player === 1) {
-    player = 2
-  } else {
-    player = 1
-  }
-}
-
-function displayNextPlayer (turn, player) {
-  turn.html('Player turn : ' + player)
-}
-
 function checkIfPlayerWon (table, pattern) {
-  let won = 0
+  let won = 0;
   if (table.find('.box1').hasClass(pattern) && table.find('.box2').hasClass(pattern) && table.find('.box3').hasClass(pattern)) {
-    won = 1
+    won = 1;
   } else if (table.find('.box1').hasClass(pattern) && table.find('.box4').hasClass(pattern) && table.find('.box7').hasClass(pattern)) {
-    won = 1
+    won = 1;
   } else if (table.find('.box1').hasClass(pattern) && table.find('.box5').hasClass(pattern) && table.find('.box9').hasClass(pattern)) {
-    won = 1
+    won = 1;
   } else if (table.find('.box4').hasClass(pattern) && table.find('.box5').hasClass(pattern) && table.find('.box6').hasClass(pattern)) {
-    won = 1
+    won = 1;
   } else if (table.find('.box7').hasClass(pattern) && table.find('.box8').hasClass(pattern) && table.find('.box9').hasClass(pattern)) {
-    won = 1
+    won = 1;
   } else if (table.find('.box2').hasClass(pattern) && table.find('.box5').hasClass(pattern) && table.find('.box8').hasClass(pattern)) {
-    won = 1
+    won = 1;
   } else if (table.find('.box3').hasClass(pattern) && table.find('.box6').hasClass(pattern) && table.find('.box9').hasClass(pattern)) {
-    won = 1
+    won = 1;
   } else if (table.find('.box3').hasClass(pattern) && table.find('.box5').hasClass(pattern) && table.find('.box7').hasClass(pattern)) {
-    won = 1
+    won = 1;
   }
   return won
 }
 
+// function getResetState (td) {
+//  if (td.hasClass('cross') || td.hasClass('circle')) {
+//    td.hasClass = null;
+//  }
+// }
+
+$(function newGame () {
+  $('#reset').on('click', function (event) {
+    createBoard()
+    $('.square').html('')
+  })
+})
+/*
 function reset (table) {
   table.find('td').each(function () {
     $(this).removeClass('circle').removeClass('cross')
-  })
+  });
 }
-
-module.export = true
+*/
